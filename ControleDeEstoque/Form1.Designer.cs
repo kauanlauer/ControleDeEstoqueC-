583cd0f88df283237f8280cc95de55bfc88e585b
Kauan Lauer <143208136+kauanlauer@users.noreply.github.com>
2024-11-25 19:42:17 -0300

    Add files via upload

---
+++ ControleDeEstoque/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ControleDeEstoque
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.dataGridViewProdutos = new System.Windows.Forms.DataGridView();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(133, 45);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantidade.TabIndex = 1;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(256, 45);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(100, 20);
+            this.txtPreco.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Quantidade";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(280, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Preço";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(12, 82);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 6;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(12, 111);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(12, 140);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 8;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // dataGridViewProdutos
+            // 
+            this.dataGridViewProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProdutos.Location = new System.Drawing.Point(372, 26);
+            this.dataGridViewProdutos.Name = "dataGridViewProdutos";
+            this.dataGridViewProdutos.Size = new System.Drawing.Size(416, 421);
+            this.dataGridViewProdutos.TabIndex = 9;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(12, 169);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtNome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.dataGridViewProdutos);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.textBox1);
+            this.Name = "txtNome";
+            this.Text = "Quantidade";
+            this.Load += new System.EventHandler(this.txtNome_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.DataGridView dataGridViewProdutos;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ ControleDeEstoque/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ControleDeEstoque
+{
+    public partial class Form1 : Form
+    {
+        private List<Produto> produtos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            produtos = new List<Produto>();
+            AtualizarDataGridView();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (ValidarEntrada())
+            {
+                var novoProduto = new Produto
+                {
+                    Id = produtos.Count > 0 ? produtos.Max(p => p.Id) + 1 : 1,
+                    Nome = textBox1.Text, // Usando textBox1 como txtNome
+                    Quantidade = int.Parse(txtQuantidade.Text),
+                    Preco = decimal.Parse(txtPreco.Text)
+                };
+
+                produtos.Add(novoProduto);
+                LimparCampos();
+                AtualizarDataGridView();
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewProdutos.SelectedRows.Count > 0 && ValidarEntrada())
+            {
+                int id = Convert.ToInt32(dataGridViewProdutos.SelectedRows[0].Cells[0].Value);
+                var produto = produtos.FirstOrDefault(p => p.Id == id);
+
+                if (produto != null)
+                {
+                    produto.Nome = textBox1.Text; // Usando textBox1 como txtNome
+                    produto.Quantidade = int.Parse(txtQuantidade.Text);
+                    produto.Preco = decimal.Parse(txtPreco.Text);
+
+                    LimparCampos();
+                    AtualizarDataGridView();
+                }
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewProdutos.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridViewProdutos.SelectedRows[0].Cells[0].Value);
+                var produto = produtos.FirstOrDefault(p => p.Id == id);
+
+                if (produto != null)
+                {
+                    produtos.Remove(produto);
+                    LimparCampos();
+                    AtualizarDataGridView();
+                }
+            }
+        }
+
+        private void dataGridViewProdutos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewProdutos.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridViewProdutos.SelectedRows[0].Cells[0].Value);
+                var produto = produtos.FirstOrDefault(p => p.Id == id);
+
+                if (produto != null)
+                {
+                    textBox1.Text = produto.Nome; // Usando textBox1 como txtNome
+                    txtQuantidade.Text = produto.Quantidade.ToString();
+                    txtPreco.Text = produto.Preco.ToString("F2");
+                }
+            }
+        }
+
+        private void AtualizarDataGridView()
+        {
+            dataGridViewProdutos.DataSource = null; // Limpa a fonte de dados
+            dataGridViewProdutos.DataSource = produtos; // Define a nova fonte de dados
+        }
+
+        private void LimparCampos()
+        {
+            textBox1.Clear(); // Usando textBox1 como txtNome
+            txtQuantidade.Clear();
+            txtPreco.Clear();
+            dataGridViewProdutos.ClearSelection();
+        }
+
+        private bool ValidarEntrada()
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                !int.TryParse(txtQuantidade.Text, out _) ||
+                !decimal.TryParse(txtPreco.Text, out _))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos corretamente.");
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
